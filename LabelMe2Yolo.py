f6d5f4f9279bb8c181bf479f6dbff2377dba36e5
Zud0taki <kosqzudotaki@gmail.com>
2022-08-12 14:13:09 +0200

    Added GUI and comments

--- LabelMe2Yolo.py
+++ LabelMe2Yolo.py
@@ -2,67 +2,81 @@ import json
 import cv2
 import glob
 
-img_input = []
-txt_input = []
 
-for image in glob.glob(
-        r"C:\Users\KIZwei\Desktop\People\*.jpg"):  # Dateinamen vom Speicherort auslesen und im Schleifenkörper in einem Array speichern
-    img_input.append(image)
-for text in glob.glob(r"C:\Users\KIZwei\Desktop\People\*.json"):
-    txt_input.append(text)
-if len(txt_input) == len(img_input):
-    for y in range(len(img_input)):
-        with open(txt_input[y], 'r') as myfile:
-            data = myfile.read()
-        img = cv2.imread(img_input[y])
-        img_size = img.shape
-        txt_name = txt_input[y]
-        txt_name = txt_name.split("\\")
-        txt_name = txt_name[5]
-        txt_name = txt_name.split(".json")
-        txt_name = txt_name[0]
-        obj = json.loads(data)
-        shape_flag = obj['shapes']
-        annotation_list = []
-        for x in range(len(shape_flag)):
-            points_flag = shape_flag[x]
-            points = points_flag['points']
-            ul = points[0]
-            lr = points[1]
-            x_center = lr[0] - ul[0]
-            if x_center < 0:
-                x_center = ul[0] - lr[0]
-            x_addition = 0
-            if ul[0] > lr[0]:
-                x_addition = lr[0]
-            else:
-                x_addition = ul[0]
-            x_center = int(x_center / 2 + x_addition)
-            x_expansion = int(lr[0] - ul[0])
-            if x_expansion < 0:
-                x_expansion = ul[0] - lr[0]
-            x_expansion = x_expansion / img_size[1]
-            x_center = x_center / img_size[1]
+def main(img_folder, json_folder, out_folder):
+    # variable declaration for the main method
+    img_input = []
+    json_input = []
 
-            y_center = lr[1] - ul[1]
-            if y_center < 0:
-                y_center = ul[1] - lr[1]
-            y_addition = 0
-            if ul[1] > lr[1]:
-                y_addition = lr[1]
-            else:
-                y_addition = ul[1]
-            y_center = int(y_center / 2 + y_addition)
+    # read images from path
+    img_folder = img_folder + "/*.jpg"
+    for image in glob.glob(img_folder):
+        img_input.append(image)
 
-            y_expansion = int(lr[1] - ul[1])
-            if y_expansion < 0:
-                y_expansion = ul[1] - lr[1]
-            y_expansion = y_expansion / img_size[0]
-            y_center = y_center / img_size[0]
-            concat = "1 " + str(x_center) + " " + str(y_center) + " " + str(x_expansion) + " " + str(y_expansion)
-            annotation_list.append(concat)
-        textfile = open(r"C:\Users\KIZwei\Desktop\People\txt" + "/" + txt_name + ".txt", "w")
-        for element in annotation_list:
-            textfile.write(str(element) + "\n")
-        textfile.close()
-        annotation_list = []
+    # read json files from path
+    json_folder = json_folder + "/*.json"
+    for text in glob.glob(json_folder):
+        json_input.append(text)
+
+    # check if img_input and json_input have the same length
+    if len(json_input) == len(img_input):
+
+        # rewrite json to txt file
+        for y in range(len(img_input)):
+            with open(json_input[y], 'r') as myfile:
+                data = myfile.read()
+            img = cv2.imread(img_input[y])
+            img_size = img.shape
+            txt_name = json_input[y]
+            txt_name = txt_name.split("\\")
+            txt_name = txt_name[1]
+            txt_name = txt_name.split(".json")
+            txt_name = txt_name[0]
+            obj = json.loads(data)
+            shape_flag = obj['shapes']
+            annotation_list = []
+            for x in range(len(shape_flag)):
+                points_flag = shape_flag[x]
+                points = points_flag['points']
+                ul = points[0]
+                lr = points[1]
+                x_center = lr[0] - ul[0]
+                if x_center < 0:
+                    x_center = ul[0] - lr[0]
+                x_addition = 0
+                if ul[0] > lr[0]:
+                    x_addition = lr[0]
+                else:
+                    x_addition = ul[0]
+                x_center = int(x_center / 2 + x_addition)
+                x_expansion = int(lr[0] - ul[0])
+                if x_expansion < 0:
+                    x_expansion = ul[0] - lr[0]
+                x_expansion = x_expansion / img_size[1]
+                x_center = x_center / img_size[1]
+
+                y_center = lr[1] - ul[1]
+                if y_center < 0:
+                    y_center = ul[1] - lr[1]
+                y_addition = 0
+                if ul[1] > lr[1]:
+                    y_addition = lr[1]
+                else:
+                    y_addition = ul[1]
+                y_center = int(y_center / 2 + y_addition)
+
+                y_expansion = int(lr[1] - ul[1])
+                if y_expansion < 0:
+                    y_expansion = ul[1] - lr[1]
+                y_expansion = y_expansion / img_size[0]
+                y_center = y_center / img_size[0]
+                concat = "1 " + str(x_center) + " " + str(y_center) + " " + str(x_expansion) + " " + str(y_expansion)
+                annotation_list.append(concat)
+            # create new txt_file at output_path
+            export_dir = (out_folder + "/" + txt_name + ".txt")
+            textfile = open(export_dir, "w")
+            for element in annotation_list:
+                textfile.write(str(element) + "\n")
+            textfile.close()
+            export_dir = ""
+            annotation_list = []
--- LabelMe2YoloGUI.py
+++ LabelMe2YoloGUI.py
@@ -4,9 +4,7 @@ from PyQt5.QtCore import QThread
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
 from PyQt5.uic import loadUi
-from ImageRotation import main
-
-
+from LabelMe2Yolo import main
 
 
 # MainWindow is the Class of the Graphical User Interface
@@ -14,7 +12,7 @@ from ImageRotation import main
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__()
-        loadUi("ImageRotation.ui", self)
+        loadUi("LabelMe2Yolo.ui", self)
         self.startButton.clicked.connect(self.mainstart)
         self.imageButton.clicked.connect(self.browseimages)
         self.textButton.clicked.connect(self.browsetext)
@@ -23,29 +21,30 @@ class MainWindow(QMainWindow):
     # define browsetext
     # used to open an explorer for the text-folder selection
     def browsetext(self):
-        textpath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Text Folder')
+        textpath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select JSON Folder', r'C:\Users\KIZwei\Desktop')
         self.textBrowser.setText(textpath)
 
     # define browseimages
     # used to open an explorer for the image-folder selection
     def browseimages(self):
-        imgpath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Image Folder')
+        imgpath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Image Folder', r'C:\Users\KIZwei\Desktop')
         self.imageBrowser.setText(imgpath)
 
     # define browseoutput
     # used to open an explorer for the output-folder selection
     def browseoutput(self):
-        outputpath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Folder')
+        outputpath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Folder', r'C:\Users\KIZwei\Desktop')
         self.outputBrowser.setText(outputpath)
 
+    # define mainstart
+    # used to start the main method in LabelMe2Yolo.py
     def mainstart(self):
         img_folder = self.imageBrowser.toPlainText()
-        txt_folder = self.textBrowser.toPlainText()
+        json_folder = self.textBrowser.toPlainText()
         out_folder = self.outputBrowser.toPlainText()
-        main(img_folder, txt_folder, out_folder)
+        main(img_folder, json_folder, out_folder)
+        mainend = self.logBrowser.setText("Finished")
 
-    def progress(self):
-        hallo = 0
 
 
 
@@ -54,6 +53,6 @@ mainwindow = MainWindow()
 widget = QtWidgets.QStackedWidget()
 widget.addWidget(mainwindow)
 widget.setFixedWidth(616)
-widget.setFixedHeight(320)
+widget.setFixedHeight(361)
 widget.show()
 sys.exit(app.exec_())
